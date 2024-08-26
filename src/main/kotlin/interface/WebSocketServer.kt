11cfa820bffff599b4dc9d60922cfc8af1d19c88
stan svec <dev@stansvec.com>
2024-08-26 11:21:38 +0200

    #1: ws server simplified

--- src/main/kotlin/interface/WebSocketServer.kt
+++ src/main/kotlin/interface/WebSocketServer.kt
@@ -7,21 +7,18 @@ import io.ktor.server.netty.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
-import kotlinx.coroutines.*
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonObject
 import java.util.concurrent.CopyOnWriteArrayList
 
-private val log = KotlinLogging.logger {}
+private val logger = KotlinLogging.logger {}
 
 typealias WebSocketMessageHandler = suspend (JsonObject) -> Unit
 
 class WebSocketServer(port: Int) {
 
     private val sensorHandlers: CopyOnWriteArrayList<WebSocketMessageHandler> = CopyOnWriteArrayList()
-
-    private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
     private val server: NettyApplicationEngine = embeddedServer(Netty, port = port) {
         install(WebSockets)
@@ -32,12 +29,10 @@ class WebSocketServer(port: Int) {
                     val receivedText = frame.readText()
                     val jsonObject = Json.parseToJsonElement(receivedText).jsonObject
                     sensorHandlers.forEach { handler ->
-                        scope.launch {
-                            try {
-                                handler(jsonObject)
-                            } catch (e: Exception) {
-                                this@embeddedServer.log.error("[sensor_handler_error] handler=[$handler]", e)
-                            }
+                        try {
+                            handler(jsonObject)
+                        } catch (e: Exception) {
+                            logger.error(e) {"[sensor_handler_error] handler=[$handler]"}
                         }
                     }
                 }
@@ -45,16 +40,15 @@ class WebSocketServer(port: Int) {
         }
     }
 
-    fun start() {
-        server.start(wait = true)
+    fun start(wait: Boolean) {
+        server.start(wait)
     }
 
     fun addSensorMessageHandler(handler: WebSocketMessageHandler) {
         sensorHandlers.add(handler)
     }
 
-    fun stop() {
-        scope.cancel()
-        server.stop(1000, 2000)
+    fun stop(gracePeriodMillis: Long, timeoutMillis: Long) {
+        server.stop(gracePeriodMillis, timeoutMillis)
     }
 }
