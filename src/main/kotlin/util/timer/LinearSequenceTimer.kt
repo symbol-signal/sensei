c258d1fe9e21045f62f05bdbac5182d2b9351ee3
stan <stan@taggstar.com>
2024-09-12 13:08:00 +0200

    #1: support for step down sequence in linear sequence timer

--- src/main/kotlin/util/timer/LinearSequenceTimer.kt
+++ src/main/kotlin/util/timer/LinearSequenceTimer.kt
@@ -30,40 +30,36 @@ class LinearSequenceTimer(
 
     suspend fun run() {
         while (true) {
-            val nowDateTime = LocalDateTime.now()
-            val cycleCalc = if (start.isBefore(end)) SameDayCycleCalc(start, end, nowDateTime) else NextDayCycleCalc(
-                start,
-                end,
-                nowDateTime
-            )
+            val now = LocalDateTime.now()
+            val cycleCalc = if (start.isBefore(end)) SameDayCycleCalc(start, end, now) else NextDayCycleCalc(start, end, now)
 
             if (cycleCalc.isOutsideCycle()) {
-                if (Duration.between(end, nowDateTime.toLocalTime()) < Duration.ofSeconds(10) && lastValue != values.last) {
+                if (Duration.between(end, now.toLocalTime()).abs() < Duration.ofSeconds(10) && lastValue != values.last) {
                     lastValue = values.last
-                    callback(SequenceUpdate(values.last, nowDateTime, cycleCalc.nextCycleStartAt().toLocalDateTime()))
+                    callback(SequenceUpdate(values.last, now, cycleCalc.nextCycleStartAt().toLocalDateTime()))
                 }
                 delay(cycleCalc.durationUntilNewCycle().toMillis())
                 continue
             }
 
-            val durationMs = cycleCalc.cycleDuration().toMillis().toDouble()
-            val range = values.last - values.first
-            val currentTime = Duration.between(start, nowDateTime).toMillis()
-            val newValue = ((currentTime / durationMs) * range + values.first).roundToInt()
+            val elapsedDurationMs = Duration.between(start, now).toMillis()
+            val cycleDurationMs = cycleCalc.cycleDuration().toMillis().toDouble()
+            val range = kotlin.math.abs(values.last - values.first)
+            val rangeValue = (elapsedDurationMs / cycleDurationMs) * range
+            val newValue = (if (values.step > 0) (values.first + rangeValue) else (values.first - rangeValue)).roundToInt()
 
-            val rateMs = durationMs / range
-            val next = (newValue - this.values.first + 1) * rateMs
+            val rateMs = cycleDurationMs / range
+            val next = if (values.step > 0) (newValue - this.values.first + 1) * rateMs else (values.first - newValue + 1) * rateMs
 
             val nextRun: LocalTime = start.plus(next.toLong(), ChronoUnit.MILLIS)
             if (lastValue != newValue) {
                 lastValue = newValue
-                callback(SequenceUpdate(newValue, nowDateTime, nextRun.atDate(nowDateTime.toLocalDate())))
+                callback(SequenceUpdate(newValue, now, nextRun.atDate(now.toLocalDate())))
             }
 
-            delay(Duration.between(nowDateTime.toLocalTime(), nextRun).toMillis())
+            delay(Duration.between(now.toLocalTime(), nextRun).toMillis())
             continue
         }
-
     }
 }
 
@@ -100,7 +96,8 @@ private class NextDayCycleCalc(start: LocalTime, end: LocalTime, now: LocalDateT
 }
 
 fun main() {
-    val phase = LinearSequenceTimer(LocalTime.of(12, 59)..LocalTime.of(13, 0), 100..200) { e -> println(e) }
+//    val phase = LinearSequenceTimer(LocalTime.of(12, 48)..LocalTime.of(12, 50), 200..400) { e -> println(e) }
+    val phase = LinearSequenceTimer(LocalTime.of(13, 4)..LocalTime.of(13, 5), 400 downTo 200) { e -> println(e) }
     runBlocking {
         phase.run()
     }
